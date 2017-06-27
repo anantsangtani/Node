eb5fc02e44df6192637a1a0aea641e762d94cd90
anantsangtani <anantsangtani1994@gmail.com>
2017-06-28 00:37:08 +0530

    final day 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# Node
-NODE tutorial
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-welcome
+var express  = require('express');
+var app      = express();
+app.get('/api/todo', function(req, res) {
+        res.sendfile('./index.html');
+    });
+ app.listen(8080);
+    console.log("App listening on port 8080");
+ 
